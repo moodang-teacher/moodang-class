cafa8b2c6b3a3b8afb6aa20f558654d8cb7d13c6
moodang <yssmi@naver.com>
2025-09-08 14:00:04 +0900

    태블릿 디자인 완성

--- js/script.js
+++ js/script.js
@@ -15,10 +15,12 @@ document.addEventListener("DOMContentLoaded", () => {
       // when window width is >= 480px
       480: {
         slidesPerView: 2,
+        spaceBetween: 20,
       },
       // when window width is >= 640px
       640: {
         slidesPerView: 3,
+        spaceBetween: 30,
       },
     },
   });
