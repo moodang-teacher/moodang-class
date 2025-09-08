ae75487a6aad7f55dcc0db61f8bff3d4f507f545
moodang <yssmi@naver.com>
2025-09-08 15:41:58 +0900

    PC 디자인 완성

--- js/script.js
+++ js/script.js
@@ -14,11 +14,16 @@ document.addEventListener("DOMContentLoaded", () => {
     breakpoints: {
       // when window width is >= 480px
       480: {
-        slidesPerView: 2,
+        slidesPerView: 1.5,
         spaceBetween: 20,
       },
       // when window width is >= 640px
       640: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+
+      1024: {
         slidesPerView: 3,
         spaceBetween: 30,
       },
